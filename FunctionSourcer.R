f5b565c056c57473d76881434747809269ad7ec2
jwillou <janna.willoughby@mac.com>
2016-09-03 07:23:15 -0400

    Add files via upload

---
+++ FunctionSourcer.R
@@ -0,0 +1,9 @@
+#Set working directory, import packages, source functions, 
+setwd(paste(directory,"/source/", sep = ''))    # set temp working directory 
+
+#import packages
+library(strataG)
+
+#source functions
+source(paste(getwd(), "/RunSims.R", sep = ''))
+
