68962c7003231ea6a19660cd8f2c01bb10ae2b7e
jwillou <janna.willoughby@mac.com>
2017-06-10 08:40:48 -0400

    Add files via upload

--- FunctionSourcer.R
+++ FunctionSourcer.R
@@ -5,11 +5,12 @@ setwd(paste(directory,"/source/", sep = ''))    # set temp working directory
 
 #install.packages("diveRsity", lib="/scratch/snyder/j/jwillou/isodrift/Rlibs/", repos='http://cran.us.r-project.org')
 #install.packages("psych", lib="/scratch/snyder/j/jwillou/isodrift/Rlibs/", repos='http://cran.us.r-project.org')
-#.libPaths("") 
+.libPaths("/scratch/snyder/j/jwillou/isodrift/Rlibs") 
 library(diveRsity)
 library(strataG)
 library(adegenet)
 
 #source functions
 source(paste(getwd(), "/RunSims.R", sep = ''))
+source(paste(getwd(), "/Advance.R", sep = ''))
 
